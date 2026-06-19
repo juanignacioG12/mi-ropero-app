a16c9cf33c1fc1abb005b7d0c384a9ff38b11da2
Juan Ignacio Garcia <juanii122803@gmail.com>
2026-06-19 18:59:17 -0300

    Fix cache Vercel

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
   }
 })
